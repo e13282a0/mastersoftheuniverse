a662c6ec15017f931c1fe5ec9e3bb2f9996f7689
cr <christian.rust@gmx.com>
2023-02-11 10:01:54 +0100

    Dies&Das

--- srv/index.js
+++ srv/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 // import socketIO from "socket.io";
 
 export default (app, http) => {
-  // app.use(express.json());
-  //
-  // app.get('/foo', (req, res) => {
-  //   res.json({msg: 'foo'});
-  // });
+  app.use(express.json());
+
+  app.get('/api', (req, res) => {
+    res.json({msg: 'foo'});
+  });
+
+
   //
   // app.post('/bar', (req, res) => {
   //   res.json(req.body);
